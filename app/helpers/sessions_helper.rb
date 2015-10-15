dbd15e4360d87096ced814dd2250b589698576e5
Victor Fernandes <victor.hmfd@gmail.com>
2015-10-15 20:09:36 -0300

    Writing comments in the methods in the helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,7 @@
 
 module SessionsHelper
 	
+  # this method logs an user
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
@@ -13,25 +14,30 @@ module SessionsHelper
     self.current_user = user
   end
 
+  # this method changes a current user
   def current_user=(user)
     @current_user = user
   end
 
+  # this method returns a current user
   def current_user
     remember_token = User.digest(cookies[:remember_token])
     return @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  # this method returns if exists a logged user
   def signed_in?
     !current_user.nil?
   end
 
+  # this method logs out an user
   def sign_out
     current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
   end
 
+  # this method compares a logged user whit another
   def current_user? user
     return user == current_user
   end
