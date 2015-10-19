b8b1cc42843bf65bce33ee2bb02a359a3609a33a
Ruan Donato <ruandonato@outlook.com>
2015-10-19 16:57:38 -0200

    Testing model team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -28,4 +28,26 @@ RSpec.describe Team, :type => :model do
     end
   end
 
+  describe "unique team" do
+    it "should be_valid" do
+      uniqueness_team = Team.new
+        uniqueness_team.id = "1"
+        uniqueness_team.name = "SiohaTec"
+        uniqueness_team.user_id = "1"
+        uniqueness_team.description = "The agile resistence!"
+        uniqueness_team.methodology = "SAFe"
+        uniqueness_team.public_to_members = true
+        uniqueness_team.email = "ssiohatecprog@gmai.com"
+        expect(uniqueness_team).to be_valid
+      end
+    end
+
+    describe "duplicated process_number" do
+      it "should not be_valid" do
+        duplicated_team = Team.new
+        duplicated_team.name= "SiohaTecproggers"
+        duplicated_team.description = "The agile resistence!"
+        expect(duplicated_team).not_to be_valid
+    end
+  end
 end
