b7df36d4d6df6442b4280a83886904f887ec0deb
Ruan Donato <ruandonato@outlook.com>
2015-11-11 11:13:51 -0200

    adding some coments in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,15 @@
 
 require 'rails_helper'
 
+#instantiate a user and team 
+
 RSpec.describe User, :type => :model do
   before do
     @user = FactoryGirl.create(:user)
     @team = FactoryGirl.create(:team, user: @user)
   end
+
+#check if a user is a member of team
 
   describe '#member_of?' do
     context 'with a given user being a member' do
@@ -21,6 +25,8 @@ RSpec.describe User, :type => :model do
       end
     end
   end
+
+#check if a team have a pending invite for users
 
   describe '#pending_to?' do
     context 'with an invite pending' do
@@ -34,6 +40,9 @@ RSpec.describe User, :type => :model do
         expect(@user.pending_to?(@team)).to eq(true)
       end
     end
+
+  #check if a team have no pending invite for users
+
     context 'with no invite pending' do
       it 'should return nil' do
         expect(@user.pending_to?(@team)).to eq(nil)
